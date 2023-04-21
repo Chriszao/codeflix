3c03b909efdad7da7d972579adf411b6f2b67d9e
Christofer Assis <chris.f.assis18@gmail.com>
2023-04-21 01:53:11 -0300

    feat: add Category class

---
+++ src/category/domain/entities.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass, field
+from datetime import datetime
+from typing import Optional
+
+
+@dataclass()
+class Category:
+
+    name: str
+    description: Optional[str] = None
+    is_active: Optional[bool] = True
+    created_at: Optional[datetime] = field(
+        default_factory=datetime.now
+    )
